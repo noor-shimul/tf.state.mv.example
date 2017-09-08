6f72a18fe841b794ab80c9feb46a28b30386d1ef
allan bailey <zirpubolci@gmail.com>
2017-09-08 20:55:11 +0000

    us-west-1a not accepting new subnets currently. so switching to 1b.

--- vpc/main.tf
+++ vpc/main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "main_vpc_subnet" {
   vpc_id     = "${aws_vpc.main.id}"
   cidr_block = "10.0.0.0/24"
 
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-1b"
 
   tags = {
     Name = "vpc-${var.environment}-${var.aws_region}"
